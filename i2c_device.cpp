75c9ee127c2f864a90c84031faf443d3f702671f
Julien FERAND <julien@dokitek.com>
2024-11-15 16:36:44 +0100

    Added I2C Interface to allow for multi device on same interface

--- i2c_device.cpp
+++ i2c_device.cpp
@@ -1,21 +1,27 @@
 #include "i2c_device.h"
 #include <cstring>
-
-void I2CDevice::write(const uint8_t reg, const uint8_t* data, const size_t size) {
+void I2CInterface::write(const uint8_t addr, const uint8_t reg, const uint8_t *data, const size_t size) {
     m_buffer[0] = reg;
     for (size_t i = 0; i < size; i++) {
         m_buffer[i + 1] = data[i];
     }
 
-    i2c_write_blocking(m_i2c, m_addr, m_buffer, size + 1, false);
+    i2c_write_blocking(m_i2c, addr, m_buffer, size + 1, false);
 
     // wipe buffer
     memset(m_buffer, 0, sizeof(m_buffer));
 }
+void I2CInterface::read(const uint8_t addr, const uint8_t reg, uint8_t *result, const size_t size) const {
+    i2c_write_blocking(m_i2c, addr, &reg, 1, true);
+    i2c_read_blocking(m_i2c, addr, result, size, false);
+}
+
+void I2CDevice::write(const uint8_t reg, const uint8_t* data, const size_t size) {
+    m_i2c.write(m_addr, reg, data, size);
+}
 
 void I2CDevice::read(const uint8_t reg, uint8_t* result, const size_t size) const {
-    i2c_write_blocking(m_i2c, m_addr, &reg, 1, true);
-    i2c_read_blocking(m_i2c, m_addr, result, size, false);
+    m_i2c.read(m_addr, reg, result, size);
 }
 
 
--- i2c_device.h
+++ i2c_device.h
@@ -9,19 +9,31 @@ struct I2CPins {
     uint8_t scl;
 };
 
-class I2CDevice {
-  private:
-     i2c_inst_t *m_i2c;
-     uint8_t m_addr;
-     uint8_t m_buffer[256];
-
-  public:
-    explicit I2CDevice(const uint8_t addr, i2c_inst_t *i2c = i2c0, const uint baudrate = 100000, const I2CPins pins = { 4, 5 }): m_i2c { i2c }, m_addr { addr }, m_buffer { 0 } {
+class I2CInterface {
+private:
+    i2c_inst_t *m_i2c;
+    uint8_t m_buffer[256];
+public:
+    explicit I2CInterface(i2c_inst_t *i2c = i2c0, const uint baudrate = 100000, const I2CPins pins = { 4, 5 }): m_i2c { i2c }, m_buffer { 0 } {
         i2c_init(i2c, baudrate);
         gpio_set_function(pins.sda, GPIO_FUNC_I2C);
         gpio_set_function(pins.scl, GPIO_FUNC_I2C);
         gpio_pull_up(pins.sda);
         gpio_pull_up(pins.scl);
+    }
+
+    void write(uint8_t addr, uint8_t reg, const uint8_t* data, size_t size = 1);
+    void read(uint8_t addr, uint8_t reg, uint8_t* result, size_t size = 1) const;
+};
+
+class I2CDevice {
+  private:
+     uint8_t m_addr;
+     I2CInterface m_i2c;
+
+  public:
+    explicit I2CDevice(const uint8_t addr, i2c_inst_t *i2c = i2c0, const uint baudrate = 100000, const I2CPins pins = { 4, 5 }): m_addr { addr } {
+        m_i2c = I2CInterface(i2c, baudrate, pins);
     }
     I2CDevice(const I2CDevice&) = delete;
     I2CDevice() = delete;
